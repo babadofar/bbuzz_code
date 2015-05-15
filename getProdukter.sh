3584eda185cd5658c8ff18928e3190a55bc6abae
Christoffer Vig <christoffer.vig@comperiosearch.com>
2015-05-15 16:18:15 +0200

    new config, mapping

--- getProdukter.sh
+++ getProdukter.sh
@@ -1,2 +1,2 @@
 wget  http://www.vinmonopolet.no/api/produkter  -O download/products.csv
- iconv -f=ISO-8859-1 -t=UTF-8 download/products.csv > download/iconproducts.csv
+iconv -f=ISO-8859-1 -t=UTF-8 download/products.csv > download/iconproducts.csv
